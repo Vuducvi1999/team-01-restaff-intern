dc4d07247e3faff9a347c97a3b9eb245e1966614
vtnguye <vtnguye@bgsu.edu>
2021-04-12 18:02:28 +0700

    create Banner DTOS + IBannerService

---
+++ BE/Domain/DTOs/Banners/BannerDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.DTOs.Banners
+{
+    public class BannerDTO
+    {
+        public string Title { get; set; }
+        public int DisplayOrder { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ BE/Domain/DTOs/Banners/DeleteBannerDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.DTOs.Banners
+{
+    public class DeleteBannerDTO
+    {
+        public Guid Id { get; set; }
+
+    }
+}
