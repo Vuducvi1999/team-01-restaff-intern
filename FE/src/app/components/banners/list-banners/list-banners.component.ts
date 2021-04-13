483bc4b277d0e125d06bb5512e2088ea6f389cf4
vtnguye <vtnguye@bgsu.edu>
2021-04-13 18:03:23 +0700

    FE: Add Delete + Create

--- FE/src/app/components/banners/list-banners/list-banners.component.ts
+++ FE/src/app/components/banners/list-banners/list-banners.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { BannerModel, ReturnMessage } from 'src/app/lib/data/models';
+import { ServerDataSource } from 'ng2-smart-table';
+import { BannerModel, PageModel, ReturnMessage, SearchPaganationDTO } from 'src/app/lib/data/models';
 import { BannersService } from 'src/app/lib/data/services';
 import { CreateBannersComponent } from '../create-banners/create-banners.component';
 
@@ -8,30 +9,25 @@ import { CreateBannersComponent } from '../create-banners/create-banners.compone
   selector: 'app-list-banners',
   templateUrl: './list-banners.component.html',
   styleUrls: ['./list-banners.component.scss'],
-  providers: [BannersService]
+  providers: []
 })
 export class ListBannersComponent implements OnInit {
   public banners = [];
-  constructor(private modalService: NgbModal,private service:BannersService) { 
-    this.service.get(null).then((res : ReturnMessage<BannerModel[]>) => {
-      if(!res.hasError)
-      {
-        console.log("banner",res.data);
-        this.banners = res.data;
-      }
-    }).catch((er) => {
-      
-      if(er.error.hasError)
-      {
-        console.log(er.error.message)
-      }
-    });
+  constructor(private modalService: NgbModal, private bannersService: BannersService) {
+    this.getBanners();
   }
-
+  source: ServerDataSource;
 
   public settings = {
+    // add: {
+    //   addButtonContent: '<i class="nb-plus"></i>',
+    //   createButtonContent: '<i class="nb-checkmark"></i>',
+    //   cancelButtonContent: '<i class="nb-close"></i>',
+    //   createConfirm: true,
+    // },
+    mode: 'external',
     actions: {
-      position: 'right'
+      position: 'right',
     },
     columns: {
       vendor: {
@@ -56,12 +52,39 @@ export class ListBannersComponent implements OnInit {
     },
   };
 
-  openCreate() {
+  getBanners() {
+    
+    this.bannersService.get(null).then((res: ReturnMessage<PageModel<BannerModel>>) => {
+      if (!res.hasError) {
+        this.banners = res.data.results;
+        console.log("banner", this.banners);
+
+      }
+    }).catch((er) => {
+
+      if (er.error.hasError) {
+        console.log(er.error.message)
+      }
+    });
+  }
+  openCreate(event: any) {
     var modalRef = this.modalService.open(CreateBannersComponent, {
       size: 'lg'
     });
-    modalRef.result.then(res => console.log(res));
+    modalRef.result.then(() => this.getBanners());
   }
+
+  delete(event: any) {
+    console.log(event);
+    let banner = event.data as BannerModel;
+    if (window.confirm("Are u sure?")) {
+      this.bannersService.delete(banner).then(res => {
+        this.getBanners();
+      });
+    }
+  }
+
+
   ngOnInit() {
   }
 
