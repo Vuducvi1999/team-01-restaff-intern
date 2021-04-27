2e13d2dc912017989e4060088a7b612448cd0ad4
bonhchi <47270741+bonhchi@users.noreply.github.com>
2021-04-27 16:56:59 +0700

    revert bugfix

--- FeUser/src/app/shared/components/menu/menu.component.ts
+++ FeUser/src/app/shared/components/menu/menu.component.ts
@@ -40,7 +40,6 @@ export class MenuComponent implements OnInit {
   async loadHeaderModel() {
    await this.headerService.getBlogs(null).then((res: any) => {
       this.headerModel.blogs = res.data;
-      console.log(res)
     })
    await this.headerService.getCategories(null).then((res: any) => {
       this.headerModel.categories = (res.data);
@@ -63,7 +62,7 @@ export class MenuComponent implements OnInit {
       this.blogsChildren.push(
         {
           title: item.title,
-          path: `/blog/${item.id}`,
+          path: '/',
           type: 'link'
         })
     });
@@ -72,7 +71,7 @@ export class MenuComponent implements OnInit {
       { title: 'home', type: 'link', path: '/home' },
       { title: 'categories', type: 'sub', active: false, children: this.categoriesChildren },
       { title: 'products', type: 'link', path: '/product' },
-      { title: 'blogs', type: 'link', path: '/blog', active: false, children: this.blogsChildren },
+      { title: 'blogs', type: 'sub', active: false, children: this.blogsChildren },
     );
   }
 
