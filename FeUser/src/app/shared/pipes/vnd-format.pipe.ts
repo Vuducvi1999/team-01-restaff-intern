dd7275a3f0c435effb643fd48346a4166e42e10e
nguyenlamquocty1412 <nguyenlamquocty1412@gmail.com>
2021-05-11 16:02:37 +0700

    modify format currency

--- FeUser/src/app/shared/pipes/vnd-format.pipe.ts
+++ FeUser/src/app/shared/pipes/vnd-format.pipe.ts
@@ -4,12 +4,17 @@ import { Pipe, PipeTransform } from "@angular/core";
 @Pipe({ name: 'vndFormat' })
 export class VndFormatPipe implements PipeTransform {
     transform(value: number) {
-        return value === null || value === undefined || !this.isNumeric(value) ? '' : value.toLocaleString('vi-VN', {
+        return value === null || value === undefined || !this.isNumeric(value) ? '' : this.numberWithCommas(value.toLocaleString('vi-VN', {
             style: 'currency',
             currency: 'VND'
-        })
+        }))
     }
     isNumeric(n) {
         return !isNaN(parseFloat(n)) && isFinite ( n );
-      }
+    }
+    numberWithCommas(n) {
+        let val = n;
+        const correctFormat = val.toString().replace(/\./g, ',')
+        return correctFormat;
+    }
 }
