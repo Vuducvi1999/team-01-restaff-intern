6ad592a43fefd6b714ee7d706ca9c3c3917b828f
Vuducvi1999 <vuducvi20@gmail.com>
2021-05-15 21:06:04 +0700

    change services component in home

--- FeUser/src/app/components/home/widgets/services/services.component.ts
+++ FeUser/src/app/components/home/widgets/services/services.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ReturnMessage } from "src/app/lib/data/models";
+import { PageContentModel } from "src/app/lib/data/models/pageContent/pageContent.model";
+import { PageContentService } from "src/app/lib/data/services/pageContent/pageContent.service";
 
 @Component({
-  selector: 'app-services',
-  templateUrl: './services.component.html',
-  styleUrls: ['./services.component.scss']
+  selector: "app-services",
+  templateUrl: "./services.component.html",
+  styleUrls: ["./services.component.scss"],
+  providers: [PageContentService],
 })
 export class ServicesComponent implements OnInit {
-
-  constructor() { }
+  pageContents: PageContentModel[] = [];
+  constructor(private pageContentService: PageContentService) {}
 
   ngOnInit(): void {
+    this.getPageContents();
   }
 
+  getPageContents() {
+    this.pageContentService
+      .getList()
+      .then((data: ReturnMessage<PageContentModel[]>) => {
+        this.pageContents = data.data;
+        console.log(data.data);
+      });
+  }
 }
