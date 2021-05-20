82da28732fd2da267a3755b707bb337a63da02a2
Vuducvi1999 <vuducvi20@gmail.com>
2021-05-20 15:51:14 +0700

    done wishlist modal

--- FeUser/src/app/shared/data/slider.ts
+++ FeUser/src/app/shared/data/slider.ts
@@ -1,27 +1,26 @@
 // Home Slider
 export let HomeSlider: any = {
-    loop: true,
-    nav: true,
-    dots: false,
-    autoplay: true,
-    autoplayTimeout: 3000,
-    autoplayHoverPause: true,
-    navContainerClass: 'owl-nav',
-    navClass: [ 'owl-prev', 'owl-next' ],
-    navText: [ '<i class="ti-angle-left"></i>', '<i class="ti-angle-right"></i>' ],
-    responsive: {
-        0: {
-            items: 1
-        },
-        400: {
-            items: 1
-        },
-        740: {
-            items: 1
-        },
-        940: {
-            items: 1
-        }
+  loop: true,
+  nav: true,
+  dots: false,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+  navContainerClass: "owl-nav",
+  navClass: ["owl-prev", "owl-next"],
+  navText: ['<i class="ti-angle-left"></i>', '<i class="ti-angle-right"></i>'],
+  responsive: {
+    0: {
+      items: 1,
+    },
+    400: {
+      items: 1,
+    },
+    740: {
+      items: 1,
+    },
+    940: {
+      items: 1,
     },
   },
 };
